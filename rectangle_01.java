f04a007ef0a448bfff195c27180ce9de6233c100
Damir Wallener <damir00@damirMBP.local>
2022-02-04 12:11:56 -0800

    Many additions, including universe, collision, etc

--- rectangle_01.java
+++ rectangle_01.java
@@ -1,4 +1,5 @@
 import java.lang.Math;
+import java.util.*;
 
 // Circle //////////////////////
 
@@ -44,6 +45,28 @@ class Circle
     }
     return false;
   }
+
+  // let's build our equality operator
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj instanceof Circle)) {
+      System.out.println("Dumped on instance test");
+      return false;
+    }
+    System.out.println("Circle properties: ");
+    System.out.println("Circle properties: ");
+
+    Circle other = (Circle) obj;
+    if ((other.center_x == this.center_x) &&
+        (other.center_y == this.center_y) &&
+        (other.radius == this.radius)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+
 
   public boolean SelfTestBounding()
   {
@@ -173,6 +196,38 @@ class Rectangle
     return 0;
   }
 
+  // let's build our equality operator
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj instanceof Rectangle)) {
+      System.out.println("Dumped on instance test");
+      return false;
+    }
+
+    Rectangle other = (Rectangle) obj;
+    if ((other.max_x == this.max_x) &&
+        (other.max_y == this.max_y) &&
+        (other.min_x == this.min_y) &&
+        (other.max_y) == (this.max_y)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // Java doesn't support operator overriding
+  // so we make our own assignment method
+  public Rectangle assign(Rectangle original)
+  {
+    Rectangle new_rect = new Rectangle(0,0,0,0);
+    new_rect.min_x = original.min_x;
+    new_rect.max_x = original.max_x;
+    new_rect.min_y = original.min_y;
+    new_rect.max_y = original.max_y;
+    return new_rect;
+  }
+
+
   // let's wrap the testing into the shape class itself, to simplify 'main
   // this is a simple test that builds it's own rectangle and chooses it's own points
 
@@ -208,7 +263,7 @@ class Rectangle
   // overload the previous method
   public boolean SelfTestBounding(Rectangle this_rect, int x, int y, boolean expected_result)
   {
-    System.out.println("TESTING: bounding test for rectang;e interior point");
+    System.out.println("TESTING: bounding test for rectangle interior point");
     boolean test_result = this_rect.IsBounding(x,y);
     if (test_result == expected_result) {
       System.out.println("Passed bounding test for arbitrary rectangle/point");
@@ -222,17 +277,9 @@ class Rectangle
   {
     System.out.println("TESTING: sameness test for rectangles");
     boolean test_result = false;
-    // tests for rectangle with identical dimensions
-    if (  (this_rect.min_x == that_rect.min_x) &
-          (this_rect.min_y == that_rect.min_y) &
-          (this_rect.max_x == that_rect.max_x) &
-          (this_rect.max_y == that_rect.max_y)) {
-      System.out.println("Passed sameness test");
-      test_result = true;
-    } else {
-      System.out.println("Failed sameness test");
-      test_result = false;
-    }
+
+    test_result = this_rect.SameAs(that_rect);
+
     if (expected_result == test_result) {
       return true;
     } else {
@@ -244,19 +291,9 @@ class Rectangle
   {
     System.out.println("TESTING: similarity test for rectangles");
     boolean test_result = false;
-    int this_distance_x = this_rect.max_x - this_rect.min_x;
-    int this_distance_y = this_rect.max_y - this_rect.min_y;
-    int that_distance_x = that_rect.max_x - that_rect.min_x;
-    int that_distance_y = that_rect.max_y - that_rect.min_y;
 
-    if (((this_distance_x == that_distance_x) & (this_distance_y == that_distance_y)) ||
-        ((this_distance_x == that_distance_y) & (this_distance_y == that_distance_x))) {
-      System.out.println("Passed similarity test");
-      test_result = true;
-    } else {
-      System.out.println("Failed similarity test");
-      test_result = false;
-    }
+    test_result = this_rect.SimilarTo(that_rect);
+
     if (expected_result == test_result) {
       return true;
     } else {
@@ -266,11 +303,65 @@ class Rectangle
 
 }
 
+// let's see if we can define a coordinate space and add basic collision detection
+class TheUniverse
+{
+  int min_edge_x;
+  int min_edge_y;
+  int max_edge_x;
+  int max_edge_y;
+
+  List<Object> objects = new ArrayList<Object>();
+
+  public TheUniverse (int min_edge_x, int min_edge_y, int max_edge_x, int max_edge_y)
+  {
+    this.min_edge_x = min_edge_x;
+    this.min_edge_y = min_edge_y;
+    this.max_edge_x = max_edge_x;
+    this.max_edge_y = max_edge_y;
+  }
+
+  // we should maintain a list of objects in the TheUniverse
+  public boolean AddShape(Object obj)
+  {
+    objects.add(obj);
+    return true;
+  }
+  // add specific methods for the two shapes
+  // these are very very basic and brute force ish
+  public boolean IsCollision(Rectangle this_rect, Rectangle that_rect)
+  {
+    boolean test_result = false;
+    for (int i = this_rect.min_x; i < this_rect.max_x; i++) {
+      for (int j = this_rect.min_y; j < this_rect.max_y; j++) {
+        // if we get any positive, it should stick through the iterations
+        test_result = test_result | that_rect.IsBounding(i,j);
+      }
+    }
+    return test_result;
+  }
+
+  public boolean IsCollision(Rectangle this_rect, Circle this_circle)
+  {
+    return false;
+  }
+
+  public boolean IsCollision(Circle this_circle, Circle that_circle)
+  {
+    return false;
+  }
+
+
+}
+
 ///////////////////////////////////////////////////////////////////////////////
 
 public class rectangle_01 {
   public static void main(String args[]) {
-    System.out.println("Here we go...");
+    // lets create our universe...
+    TheUniverse OurUniverse = new TheUniverse(0,0,1000,1000);
+
+    System.out.println("Here we go...universe of (0,0,1000,1000) created...");
 
     boolean test_result = false; // set to fail until a pass is earned
 
@@ -309,7 +400,7 @@ public class rectangle_01 {
     Circle circle_12 = new Circle(circle_11);
     System.out.println("Copy construct a circle...");
     circle_12.Properties();
-    if (circle_12 == circle_11) {
+    if (circle_12.equals(circle_11)) {
       System.out.println("PASSED: Circle copy constructor test");
     } else {
       System.out.println("FAILED: Circle copy constructor test");
@@ -317,11 +408,12 @@ public class rectangle_01 {
     Rectangle rect_12 = new Rectangle(rect_11);
     System.out.println("Copy construct a rectangle...");
     rect_12.Properties();
-    if (rect_12 == rect_11) {
-      System.out.println("PASSED: Circle copy constructor test");
+    if (rect_12.equals(rect_11)) {
+      System.out.println("PASSED: Rectangle copy constructor test");
     } else {
-      System.out.println("FAILED: Circle copy constructor test");
+      System.out.println("FAILED: Rectangle copy constructor test");
     }
+
 
     // sameness test
     Rectangle rect_1 = new Rectangle(0, 0, 100, 100);
@@ -341,6 +433,53 @@ public class rectangle_01 {
       System.out.println("PASSED: Rectangle sameness tests");
     } else {
       System.out.println("FAILED: Rectangle sameness tests");
+    }
+
+    // test the assign method
+    System.out.println("Testing Assignment: ");
+    Rectangle rect_99 = new Rectangle (0,0,0,0);
+    Rectangle rect_98 = new Rectangle (0,0,100,100);
+    // this should fail the assignment test
+    if (rect_1.SelfTestSameness(rect_99, rect_98, false)) {
+      System.out.println("PASSED: Rectangle sameness tests");
+    } else {
+      System.out.println("FAILED: Rectangle sameness tests");
+    }
+    // now do the assign
+    rect_98 = rect_1.assign(rect_99);
+    // this should pass the assignment test
+    if (rect_1.SelfTestSameness(rect_99, rect_98, true)) {
+      System.out.println("PASSED: Rectangle sameness tests");
+    } else {
+      System.out.println("FAILED: Rectangle sameness tests");
+    }
+
+    // Add objects to our universe...
+    OurUniverse.AddShape(rect_99);
+    OurUniverse.AddShape(rect_11);
+
+    // Do a collion test between two rectangles
+    // this should be an obvious collision
+    System.out.println("Collision testing...");
+
+    System.out.println("This should collide");
+    rect_99.ChangeBounds(20, 20, 40, 40);
+    rect_98.ChangeBounds(25, 25, 35, 35);
+    test_result = OurUniverse.IsCollision(rect_99, rect_98);
+    if (test_result) {
+      System.out.println("Collision!");
+    } else {
+      System.out.println("No Collision!");
+    }
+
+    rect_99.ChangeBounds(20, 20, 40, 40);
+    rect_98.ChangeBounds(50, 50, 75, 75);
+    System.out.println("This should not collide");
+    test_result = OurUniverse.IsCollision(rect_99, rect_99);
+    if (!test_result) {
+      System.out.println("Collision!");
+    } else {
+      System.out.println("No Collision!");
     }
 
 
