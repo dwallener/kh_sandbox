57aff4307f3cecdc4876b0179582c6a1d109cc07
Damir Wallener <damir00@damirMBP.local>
2022-02-04 10:16:46 -0800

    moved testing methods inside the shape classes

--- rectangle_01.java
+++ rectangle_01.java
@@ -45,6 +45,46 @@ class Circle
     return false;
   }
 
+  public boolean SelfTestBounding()
+  {
+    Circle circle_1 = new Circle(0,0,100);
+    boolean testResult = false; // default to false
+
+    // Interior point - this should generate a TRUE response
+    if (circle_1.IsBounding(8,9)) {
+      System.out.println("Passed bounding test for interior point");
+      testResult = true;
+    } else {
+      System.out.println("Failed bounding test for interior point");
+    }
+
+    // exterior point - this should generate a TRUE response
+    if (circle_1.IsBounding(108,109)) {
+      System.out.println("Failed bounding test for exterior point");
+    } else {
+      System.out.println("Passed bounding test for exterior point");
+      // only pass this if the previous test(s) have passed
+      if (testResult) {
+        testResult = true;
+      }
+    }
+    return testResult;
+  }
+
+  // this test allows arbitrary input for SelfTestBounding
+  // overload the previous method
+  public boolean SelfTestBounding(Circle this_circle, int x, int y, boolean expected_result)
+  {
+    boolean test_result = this_circle.IsBounding(x,y);
+    if (test_result == expected_result) {
+      System.out.println("Passed bounding test for arbitrary circle/point");
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+
 }
 
 
@@ -53,10 +93,10 @@ class Circle
 class Rectangle
 {
   // properties of the rectangle
-  int min_x;
-  int max_x;
-  int min_y;
-  int max_y;
+  int min_x = 0;
+  int max_x = 0;
+  int min_y = 0;
+  int max_y = 0;
 
   // constructor
   public Rectangle (int min_x, int min_y, int max_x, int max_y)
@@ -133,6 +173,97 @@ class Rectangle
     return 0;
   }
 
+  // let's wrap the testing into the shape class itself, to simplify 'main
+  // this is a simple test that builds it's own rectangle and chooses it's own points
+
+  public boolean SelfTestBounding()
+  {
+    System.out.println("TESTING: bounding test for rectang;e interior point");
+
+    Rectangle rect_1 = new Rectangle(0,0,100,100);
+    boolean testResult = false; // default to false
+
+    // Interior point - this should generate a TRUE response
+    if (rect_1.IsBounding(8,9)) {
+      System.out.println("Passed bounding test for interior point");
+      testResult = true;
+    } else {
+      System.out.println("Failed bounding test for interior point");
+    }
+
+    // exterior point - this should generate a TRUE response
+    if (rect_1.IsBounding(108,109)) {
+      System.out.println("Failed bounding test for exterior point");
+    } else {
+      System.out.println("Passed bounding test for exterior point");
+      // only pass this if the previous test(s) have passed
+      if (testResult) {
+        testResult = true;
+      }
+    }
+    return testResult;
+  }
+
+  // this test allows arbitrary input for SelfTestBounding
+  // overload the previous method
+  public boolean SelfTestBounding(Rectangle this_rect, int x, int y, boolean expected_result)
+  {
+    System.out.println("TESTING: bounding test for rectang;e interior point");
+    boolean test_result = this_rect.IsBounding(x,y);
+    if (test_result == expected_result) {
+      System.out.println("Passed bounding test for arbitrary rectangle/point");
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public boolean SelfTestSameness(Rectangle this_rect, Rectangle that_rect, boolean expected_result)
+  {
+    System.out.println("TESTING: sameness test for rectangles");
+    boolean test_result = false;
+    // tests for rectangle with identical dimensions
+    if (  (this_rect.min_x == that_rect.min_x) &
+          (this_rect.min_y == that_rect.min_y) &
+          (this_rect.max_x == that_rect.max_x) &
+          (this_rect.max_y == that_rect.max_y)) {
+      System.out.println("Passed sameness test");
+      test_result = true;
+    } else {
+      System.out.println("Failed sameness test");
+      test_result = false;
+    }
+    if (expected_result == test_result) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public boolean SelfTestSimilarity(Rectangle this_rect, Rectangle that_rect, boolean expected_result)
+  {
+    System.out.println("TESTING: similarity test for rectangles");
+    boolean test_result = false;
+    int this_distance_x = this_rect.max_x - this_rect.min_x;
+    int this_distance_y = this_rect.max_y - this_rect.min_y;
+    int that_distance_x = that_rect.max_x - that_rect.min_x;
+    int that_distance_y = that_rect.max_y - that_rect.min_y;
+
+    if (((this_distance_x == that_distance_x) & (this_distance_y == that_distance_y)) ||
+        ((this_distance_x == that_distance_y) & (this_distance_y == that_distance_x))) {
+      System.out.println("Passed similarity test");
+      test_result = true;
+    } else {
+      System.out.println("Failed similarity test");
+      test_result = false;
+    }
+    if (expected_result == test_result) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -141,84 +272,75 @@ public class rectangle_01 {
   public static void main(String args[]) {
     System.out.println("Here we go...");
 
-    // let's start with circles...
-    System.out.println("Create a circle object at 5,5 with radius 10");
-    Circle circle_1 = new Circle(50,50,10);
-    circle_1.Properties();
+    boolean test_result = false; // set to fail until a pass is earned
+
+    // Rectangle bounding test
+    // first method
+    Rectangle rect_10 = new Rectangle(0,0,100,100);
+    Rectangle rect_11 = new Rectangle(20, 20, 50, 50);
+    test_result = rect_10.SelfTestBounding();
+    // second method - precompute the expected result, look for the match
+    test_result = test_result & rect_10.SelfTestBounding(rect_11, 60, 60, false);
+    test_result = test_result & rect_10.SelfTestBounding(rect_11, 23, 44, true);
+
+    if (test_result) {
+      System.out.println("PASSED: Rectangle bounding tests");
+    } else {
+      System.out.println("FAILED: Rectangle bounding tests");
+    }
+
+    // Circt bounding test
+    // first method
+    Circle circle_10 = new Circle(0,0,100);
+    Circle circle_11 = new Circle(20, 20, 10);
+    // simple test
+    test_result = circle_10.SelfTestBounding();
+    // second method - precompute the expected result, look for the match
+    test_result = test_result & circle_10.SelfTestBounding(circle_11, 60, 60, false);
+    test_result = test_result & circle_10.SelfTestBounding(circle_11, 23, 24, true);
+
+    if (test_result) {
+      System.out.println("PASSED: Circle bounding tests");
+    } else {
+      System.out.println("FAILED: Circle bounding tests");
+    }
 
     // test out the copy constructor
-    Circle circle_2 = new Circle(circle_1);
-    System.out.println("Copy construct another one...");
-    circle_2.Properties();
-
-    // test the bounding test...
-    System.out.println("Test if point 8,9 is inside the circle");
-    if (circle_1.IsBounding(58,59)) {
-      System.out.println("Yep!");
+    Circle circle_12 = new Circle(circle_11);
+    System.out.println("Copy construct a circle...");
+    circle_12.Properties();
+    if (circle_12 == circle_11) {
+      System.out.println("PASSED: Circle copy constructor test");
     } else {
-      System.out.println("Nope!");
+      System.out.println("FAILED: Circle copy constructor test");
     }
-    System.out.println("Test if point 118,129 is inside the circle");
-    if (circle_1.IsBounding(68,69)) {
-      System.out.println("Yep!");
+    Rectangle rect_12 = new Rectangle(rect_11);
+    System.out.println("Copy construct a rectangle...");
+    rect_12.Properties();
+    if (rect_12 == rect_11) {
+      System.out.println("PASSED: Circle copy constructor test");
     } else {
-      System.out.println("Nope!");
-    }
-
-    // Move on to rectangles...
-    System.out.println("Creating a rectangle object at (0,0) to (100,100)");
-    Rectangle rect_1 = new Rectangle(0,0,100,100);
-    rect_1.Properties();
-
-    // test the bounding test...
-    System.out.println("Test if point 8,9 is inside the rectangle");
-    if (rect_1.IsBounding(8,9)) {
-      System.out.println("Yep!");
-    } else {
-      System.out.println("Nope!");
-    }
-    System.out.println("Test if point 118,129 is inside the rectangle");
-    if (rect_1.IsBounding(118,129)) {
-      System.out.println("Yep!");
-    } else {
-      System.out.println("Nope!");
+      System.out.println("FAILED: Circle copy constructor test");
     }
 
-    // test for sameness
-    // create 2nd rectangle - same as first using the normal constructor
-    Rectangle rect_2 = new Rectangle(0,0,100,100);
-    // create 3rd rectangle using the copy constructor
-    Rectangle rect_3 = new Rectangle(rect_2);
-    rect_2.Properties();
-    rect_3.Properties();
-    // change rectangle 3
-    rect_3.ChangeBounds(0, 0, 50, 100);
-    rect_3.Properties();
-    // change rectangle 2
-    rect_2.ChangeBounds(0, 0, 100, 50);
-    rect_2.Properties();
-    // change rectangle 1
-    rect_1.ChangeBounds(0, 0, 50, 100);
-    rect_1.Properties();
+    // sameness test
+    Rectangle rect_1 = new Rectangle(0, 0, 100, 100);
+    Rectangle rect_2 = new Rectangle(0, 0, 50, 100);
+    Rectangle rect_3 = new Rectangle(0, 0, 100, 50);
+    Rectangle rect_4 = new Rectangle(0, 0, 100, 100);
+    // rect_1 and rect_4 should pass the same test
+    // rect_2 and rect_3 should fail the same test
+    // rect_2 and rect_3 should pass the similar test
+    // reset expectations
+    test_result = false;
+    test_result = rect_1.SelfTestSameness(rect_1, rect_4, true);
+    test_result = test_result & rect_1.SelfTestSameness(rect_2, rect_3, false);
+    test_result = test_result & rect_1.SelfTestSimilarity(rect_2, rect_3, true);
 
-    // now rectangles 2 and 3 should not be the same but they should be similar
-    System.out.println("Test if Rectangles 2 & 3 are the same");
-    if (rect_2.SameAs(rect_3)) {
-      System.out.println("Yep!");
+    if (test_result) {
+      System.out.println("PASSED: Rectangle sameness tests");
     } else {
-      System.out.println("Nope!");
-    }
-    System.out.println("Test if Rectangles 2 & 3 are similar");
-    if (rect_2.SimilarTo(rect_3)) {
-      System.out.println("Yep!");
-    } else {
-      System.out.println("Nope!");
-    }
-    System.out.println("Test if Rectangles 1 & 3 are the same");
-    if (rect_1.SameAs(rect_3)) {
-      System.out.println("Yep!");
-    } else {
-      System.out.println("Nope!");
+      System.out.println("FAILED: Rectangle sameness tests");
     }
 
 
